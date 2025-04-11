3fbfedf4b95a3634265ffab8ce8d6d2e09609c0d
Froloveee3 <vadim.frolov.chelny02@gmail.com>
2025-04-12 01:00:11 +0300

    Add Task1: basic data types demonstration

--- src/tasks/Task1.java
+++ src/tasks/Task1.java
@@ -4,5 +4,17 @@ public class Task1 {
     public static void main(String[] args) {
         // TODO: Создай переменные всех базовых типов данных
         // int, double, boolean, String и выведи их в консоль
+
+        int intValue = 10;
+        double doubleValue = 1.23;
+        boolean booleanValue = true;
+        char charValue = 'A';
+        String stringValue = "Hello, Java!";
+
+        System.out.println("intValue: " + intValue);
+        System.out.println("doubleValue: " + doubleValue);
+        System.out.println("booleanValue: " + booleanValue);
+        System.out.println("charValue: " + charValue);
+        System.out.println("stringValue: " + stringValue);
     }
 }
