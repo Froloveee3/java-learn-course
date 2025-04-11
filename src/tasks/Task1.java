c9d090f2d981a435e61b5450b23be41eafab9d51
Froloveee3 <vadim.frolov.chelny02@gmail.com>
2025-04-12 00:53:21 +0300

    Init

---
+++ src/tasks/Task1.java
@@ -0,0 +1,8 @@
+package tasks;
+
+public class Task1 {
+    public static void main(String[] args) {
+        // TODO: Создай переменные всех базовых типов данных
+        // int, double, boolean, String и выведи их в консоль
+    }
+}
