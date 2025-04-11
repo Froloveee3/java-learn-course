82cb5700d23b1021bc384873897e2b8a232f549b
Froloveee3 <vadim.frolov.chelny02@gmail.com>
2025-04-12 01:10:32 +0300

    Add Task2: conditionals demo

--- src/tasks/Task2.java
+++ src/tasks/Task2.java
@@ -1,5 +1,33 @@
 package tasks;
 
 public class Task2 {
-    
+    public static void main(String[] args) {
+        int score = 100;
+
+        if (score >= 90) {
+            System.out.println("Great");
+        } else if (score >= 80) {
+            System.out.println("Good");
+        } else if (score >= 70) {
+            System.out.println("Not bad");
+        } else {
+            System.out.println("Bad");
+        }
+
+        String day = "Monday";
+
+        switch (day) {
+            case "Monday":
+                System.out.println("Start of work weak");
+                break;
+
+            case "Пятница":
+                System.out.println("End of work weak");
+                break;
+
+            default:
+                System.out.println("Basic day");
+                break;
+        }
+    }
 }
